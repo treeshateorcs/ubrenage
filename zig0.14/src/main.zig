747fac23b34e9e6913b7d01f0d993409a1790ada
tho <trees@hateorcs.com>
2024-11-13 18:59:59 -0500

    rewrite zig version in 0.14

---
+++ zig0.14/src/main.zig
@@ -0,0 +1,55 @@
+const std = @import("std");
+const nouns = @embedFile("nouns");
+const adjectives = @embedFile("adjectives");
+
+pub fn main() !void {
+    const random = std.crypto.random;
+    var args_iter = std.process.args();
+    _ = args_iter.next();
+    const ra = @mod(random.int(u8), 26) + 97;
+    const rand_letter: []const u8 = &[_]u8{ra};
+
+    const rand_letter_number_string: []const u8 = args_iter.next() orelse rand_letter;
+    for (rand_letter_number_string) |letter| {
+        if (letter < 97 or letter > 122) {
+            usage();
+            std.process.exit(1);
+        }
+        std.debug.print("{s} {s}\n", .{ randomWordFromList(adjectives, letter), randomWordFromList(nouns, letter) });
+    }
+}
+
+fn randomWordFromList(list: []const u8, letter_number: i32) []const u8 {
+    const rand = std.crypto.random;
+    var it = std.mem.splitAny(u8, list, "\n");
+    var count: u32 = 0;
+    var alphabet_count = [_]i32{0} ** 26;
+    while (it.next()) |noun| {
+        count += 1;
+        if (noun.len > 0) {
+            alphabet_count[noun[0] - 'a'] += 1;
+        }
+    }
+    const rand_int = @mod(rand.int(i32), alphabet_count[@intCast(letter_number - 'a')]);
+    var random_word_number: i32 = 0;
+    for (alphabet_count, 0..) |letter, index| {
+        if (index != letter_number - 'a') {
+            random_word_number += letter;
+        } else {
+            break;
+        }
+    }
+    random_word_number += rand_int;
+    var it2 = std.mem.splitAny(u8, list, "\n");
+    while (it2.next()) |noun| {
+        random_word_number -= 1;
+        if (random_word_number == 0) {
+            return noun;
+        }
+    }
+    return "";
+}
+
+fn usage() void {
+    std.debug.print("usage: ubrenage [abcdefghijklmnopqrstuvwxyz...]\n", .{});
+}
